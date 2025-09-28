61f09035ffebe8e3aef47a3523178537c06941d7
gptfleet <gptfleet@gmail.com>
2025-09-28 02:52:35 +0000

    Update

--- src/components/profile/ProfileEditForm.tsx
+++ src/components/profile/ProfileEditForm.tsx
@@ -1,5 +1,4 @@
 'use client';
-/* eslint-disable @next/next/no-img-element */
 
 import { useEffect, useState } from 'react';
 import ChangeAvatarModal from '@/components/profile/ChangeAvatarModal';
